04b88456e30dfbb1053cb27e0d2f33db09e083b5
wangqb1129@163.com <wangqb1129@163.com>
2019-02-21 20:52:25 +0800

    zhushi

--- src/test/java/com/criss/wang/SpringbootNioApplicationTests.java
+++ src/test/java/com/criss/wang/SpringbootNioApplicationTests.java
@@ -38,6 +38,11 @@ import org.springframework.test.context.junit4.SpringRunner;
  * 非直接缓冲区：通过 allocate() 方法分配缓冲区，将缓冲区建立在 JVM 的内存中
  * 直接缓冲区：通过 allocateDirect() 方法分配直接缓冲区，将缓冲区建立在物理内存中。可以提高效率
  */
+/**
+ * 
+ * @author Administrator
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootNioApplicationTests {
